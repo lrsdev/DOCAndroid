5cc81db7957943fe72e155f2b9330373e85abbde
Rickie Kewene <kewenrj1@student.op.ac.nz>
2015-10-01 13:27:15 +1300

    Donate page done, links working

--- app/src/main/java/io/github/lrsdev/dogbeaches/DonateFragment.java
+++ app/src/main/java/io/github/lrsdev/dogbeaches/DonateFragment.java
@@ -9,37 +9,56 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 
-import io.github.lrsdev.dogbeaches.R;
 /**
  * Created by rickiekewene on 29/09/15.
  */
 public class DonateFragment extends Fragment
 {
+    private ImageButton yept;
+    private ImageButton sea;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        View view = inflater.inflate(R.layout.fragment_donate, container, false);
-/*
-        //Yellow-Eyed Penguin Trust Intent(Open browser to donation url page)
-        ImageButton yept = (ImageButton)findviewById(R.id.yept_icon);
 
-        yept.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                String url = "http//www.yellow-eyedpenguin.co.nz/passion/support-the-trusts-work/become-a-trust-supporter";
-                Intent urlIntent1 = new Intent();
-                urlIntent1.setAction(Intent.ACTION_VIEW);
-                urlIntent1.setData(Uri.parse(url));
-                startActivity(urlIntent1);
-            }
-        }); */
+        View view = inflater.inflate(R.layout.fragment_donate,container, false);
+
+        //Yellow-Eyed Penguin Trust & NZ Sealion Trust Intent(Open browser to donation url page)
+        yept = (ImageButton)view.findViewById(R.id.yept_icon);
+        sea = (ImageButton)view.findViewById(R.id.sea_icon);
+
+        yept.setOnClickListener(clickListener);
+        sea.setOnClickListener(clickListener);
 
         return view;
     }
 
-
+    private View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v)
+        {
+            switch (v.getId())
+            {
+                case R.id.yept_icon:
+                    Uri url1 = Uri.parse("http://www.yellow-eyedpenguin.org.nz/passion/support-the-trusts-work");
+                    Intent launchBrowser1 = new Intent(Intent.ACTION_VIEW, url1);
+                    startActivity(launchBrowser1);
+                    break;
+                case R.id.sea_icon:
+                    Uri url2 = Uri.parse("http://www.sealiontrust.org.nz/");
+                    Intent launchBrowser2 = new Intent(Intent.ACTION_VIEW, url2);
+                    startActivity(launchBrowser2);
+                    break;
+            }
+        }
+    };
 
     @Override
     public void onResume()
